c69921e7617b90384835da623b72b9e03ba39f20
afrontend <autoscripts@gmail.com>
2020-05-05 09:08:18 +0900

    Update toProperty for testing

--- src/csv-property.js
+++ src/csv-property.js
@@ -1,5 +1,22 @@
-function toProperty() {
-  return {}
+function toProperty(csvStr) {
+  if (!csvStr) return {}
+
+  const ary = csvStr.split(',').map(a => a.trim())
+  if (ary.length === 0) return {}
+
+  let obj = {};
+  let tmpObj =  obj;
+
+  const last = ary.pop()
+  ary.forEach((prop, index) => {
+     if (ary.length - 1 === index) {
+       tmpObj[prop] = last
+     } else {
+       obj[prop] = {}
+       tmpObj = obj[prop]
+     }
+  });
+  return obj
 }
 
 module.exports = {
--- test/csv-property.spec.js
+++ test/csv-property.spec.js
@@ -2,7 +2,7 @@ const { toProperty } = require('../src/csv-property.js');
 const assert = require('assert');
 
 describe('csv-property', function () {
-  it('csv string to JSON ', function (done) {
+  it('csv string to JSON ', function () {
     assert.deepEqual(toProperty("name, color, red"), { name: { color: 'red' } });
   });
 });
