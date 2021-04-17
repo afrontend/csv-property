b8f0603cb192a6f657f49a046087b4b6f5f7af6c
afrontend <autoscripts@gmail.com>
2021-04-17 14:36:51 +0900

    Update test text

--- test/csv-property.spec.js
+++ test/csv-property.spec.js
@@ -7,28 +7,28 @@ describe('csv-property', function () {
   })
   it('generate JSON using an array of CSV string', function () {
     assert.deepEqual(toObject([
-      'popup, error, dupError, dupplicated id',
-      'popup, error, authError, no auth'
+      'key1, key2, key3, dupplicated id',
+      'key1, key2, key4, no auth'
     ]), {
-      popup: {
-        error: {
-          dupError: 'dupplicated id',
-          authError: 'no auth'
+      key1: {
+        key2: {
+          key3: 'dupplicated id',
+          key4: 'no auth'
         }
       },
     })
   })
   it('convert object to an array of CSV string', function () {
     assert.deepEqual(toCSVString({
-      popup: {
-        error: {
-          dupError: 'dupplicated id',
-          authError: 'no auth'
+      key1: {
+        key2: {
+          key3: 'dupplicated id',
+          key4: 'no auth'
         }
       },
     }), [
-      'popup, error, dupError, dupplicated id',
-      'popup, error, authError, no auth'
+      'key1, key2, key3, dupplicated id',
+      'key1, key2, key4, no auth'
     ])
   })
   it('generate JSON using CSV string with delimiter', function () {
@@ -36,37 +36,37 @@ describe('csv-property', function () {
   })
   it('generate JSON using an array of CSV string with delimiter', function () {
     assert.deepEqual(toObject([
-      'popup; error; dupError; dupplicated id',
-      'popup; error; authError; no auth'
+      'key1; key2; key3; dupplicated id',
+      'key1; key2; key4; no auth'
     ], ';'), {
-      popup: {
-        error: {
-          dupError: 'dupplicated id',
-          authError: 'no auth'
+      key1: {
+        key2: {
+          key3: 'dupplicated id',
+          key4: 'no auth'
         }
       },
     })
   })
   it('convert object to an array of CSV string with delimiter', function () {
     assert.deepEqual(toCSVString({
-      popup: {
-        error: {
-          dupError: 'dupplicated id',
-          authError: 'no auth'
+      key1: {
+        key2: {
+          key3: 'dupplicated id',
+          key4: 'no auth'
         }
       },
     }, ';'), [
-      'popup; error; dupError; dupplicated id',
-      'popup; error; authError; no auth'
+      'key1; key2; key3; dupplicated id',
+      'key1; key2; key4; no auth'
     ])
   })
   it('generate JSON using an array of CSV string with delimiter (CSV string can be 0, 1, ...)', function () {
     assert.deepEqual(toObject([
-      'popup; error; 0; dupplicated id',
-      'popup; error; 1; no auth'
+      'key1; key2; 0; dupplicated id',
+      'key1; key2; 1; no auth'
     ], ';'), {
-      popup: {
-        error: {
+      key1: {
+        key2: {
           0: 'dupplicated id',
           1: 'no auth'
         }
